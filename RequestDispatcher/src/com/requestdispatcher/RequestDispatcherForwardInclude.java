ac3791b9ea0e1cc4030fbaae6af8d7f0f0036766
adharsh-yesugari <yesugariadharshreddy150603@gmail.com>
2025-11-28 17:16:36 +0530

    added session

--- RequestDispatcher/src/com/requestdispatcher/RequestDispatcherForwardInclude.java
+++ RequestDispatcher/src/com/requestdispatcher/RequestDispatcherForwardInclude.java
@@ -6,6 +6,7 @@ import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
 
 import java.io.IOException;
 import java.io.PrintWriter;
@@ -14,6 +15,11 @@ import java.io.PrintWriter;
 public class RequestDispatcherForwardInclude extends HttpServlet {
     public void service(HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException {
         System.out.println("Controller in First Servlet");
+        String userName = request.getParameter("userName");
+        String userCity = request.getParameter("userCity");
+        HttpSession session = request.getSession();
+        session.setAttribute("userName", userName);
+        session.setAttribute("userCity", userCity);
         PrintWriter writer = response.getWriter();
         writer.println("<h1> First Servlet</h1>");
         RequestDispatcher rd = request.getRequestDispatcher("/secondServlet");
